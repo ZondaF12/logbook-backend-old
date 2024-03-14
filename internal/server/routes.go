d153bc1fed5ba3db252c78358b880349b2938a53
zondaf12 <ruaridhbell73@googlemail.com>
2024-03-14 23:08:55 +0000

    Added Swagger spec

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -3,22 +3,42 @@ package server
 import (
 	"net/http"
 
+	_ "logbook-backend/cmd/api/docs"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	echoSwagger "github.com/swaggo/echo-swagger" // echo-swagger middleware
 )
 
+// @title			Logbook Backend API
+// @version		1.0
+// @description	Backend for the mobile app `Logbook` made with Echo and Golang
+// @host			localhost:8080
+// @BasePath		/api/v1
 func (s *Server) RegisterRoutes() http.Handler {
 	e := echo.New()
-	e.Use(middleware.Logger())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
 	e.Use(middleware.Recover())
 
-	e.GET("/", s.HelloWorldHandler)
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
-	e.GET("/health", s.healthHandler)
+	// grouped routes
+	g := e.Group("/api/v1")
+	g.GET("/", s.HelloWorldHandler)
+	g.GET("/health", s.healthHandler)
 
 	return e
 }
 
+// HelloWorld godoc
+//
+//	@Summary		Hello World Route
+//	@Description	returns `Hello World`
+//	@Tags			default
+//	@Success		200
+//	@Router			/ [get]
 func (s *Server) HelloWorldHandler(c echo.Context) error {
 	resp := map[string]string{
 		"message": "Hello World",
@@ -27,6 +47,13 @@ func (s *Server) HelloWorldHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, resp)
 }
 
+// Health godoc
+//
+//	@Summary		Returns the database health
+//	@Description	get the database health
+//	@Tags			default
+//	@Success		200
+//	@Router			/health [get]
 func (s *Server) healthHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, s.db.Health())
 }
